690215d1fcb39375e2faf819a736e59b0ded5d55
Simon Eriksson <sim.eriksson@outlook.com>
2023-07-29 22:06:43 +0200

    Remove more unneeded stuff from template

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,12 @@ import electron from "vite-plugin-electron";
 import renderer from "vite-plugin-electron-renderer";
 import pkg from "./package.json";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   rmSync("dist-electron", { recursive: true, force: true });
 
   const isServe = command === "serve";
   const isBuild = command === "build";
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
+  const sourcemap = isServe;
 
   return {
     resolve: {
@@ -24,14 +23,9 @@ export default defineConfig(({ command }) => {
       react(),
       electron([
         {
-          // Main-Process entry file of the Electron App.
           entry: "electron/main.ts",
           onstart(options) {
-            if (process.env.VSCODE_DEBUG) {
-              console.log(/* For `.vscode/.debug.script.mjs` */ "[startup] Electron App");
-            } else {
-              options.startup();
-            }
+            options.startup();
           },
           vite: {
             build: {
@@ -47,13 +41,11 @@ export default defineConfig(({ command }) => {
         {
           entry: "electron/preload.ts",
           onstart(options) {
-            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-            // instead of restarting the entire Electron App.
             options.reload();
           },
           vite: {
             build: {
-              sourcemap: sourcemap ? "inline" : undefined, // #332
+              sourcemap: sourcemap ? "inline" : undefined,
               minify: isBuild,
               outDir: "dist-electron",
               rollupOptions: {
@@ -63,18 +55,8 @@ export default defineConfig(({ command }) => {
           },
         },
       ]),
-      // Use Node.js API in the Renderer-process
       renderer(),
     ],
-    server:
-      process.env.VSCODE_DEBUG &&
-      (() => {
-        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
-        return {
-          host: url.hostname,
-          port: +url.port,
-        };
-      })(),
     clearScreen: false,
   };
 });
